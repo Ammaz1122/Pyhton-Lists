113cdebb90419f818a2a0ac2e41723fa4a5b0303
Ammaz <mmzhmd@gmail.com>
2025-10-25 10:48:17 +0500

    ListBasics

---
+++ List.py
@@ -0,0 +1,11 @@
+student_grades = [85,90,78,95,82]
+
+student_grades.insert(2,90)
+print(student_grades)
+
+student_grades.append(100)
+print(student_grades)
+
+student_grades.remove(82)  # Remove the specific element that we are included in 
+print(student_grades)
+
